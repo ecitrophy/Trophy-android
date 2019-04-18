73d01839669b7b973baec453bff34a129193b999
Sebastián Reyes <sebastianreyes01@hotmail.com>
2019-04-17 23:33:01 -0500

    login and signup

---
+++ app/src/main/java/com/edu/eci/ieti/trophy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.edu.eci.ieti.trophy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}

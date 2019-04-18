0d2202958098c7f756a9fe9a1af27f9b3a65812a
Sebastián Reyes <sebastianreyes01@hotmail.com>
2019-04-18 14:38:11 -0500

    drawer, lobby and logout

--- app/src/main/java/com/edu/eci/ieti/trophy/MainActivity.java
+++ app/src/main/java/com/edu/eci/ieti/trophy/MainActivity.java
@@ -2,20 +2,30 @@ package com.edu.eci.ieti.trophy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Activity main;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main = this;
     }
 
     public void signUp(View view) {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
+    }
+
+    public void lobby(View view) {
+        Intent intent = new Intent(this, LobbyActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/edu/eci/ieti/trophy/SignUpActivity.java
+++ app/src/main/java/com/edu/eci/ieti/trophy/SignUpActivity.java
@@ -2,7 +2,9 @@ package com.edu.eci.ieti.trophy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void lobby(View view) {
+        Intent intent = new Intent(this, LobbyActivity.class);
+        startActivity(intent);
+        MainActivity.main.finish();
+        finish();
     }
 }
